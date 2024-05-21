14a9e8ca86eeb322db2653c271e220a1853acf61
MatMon28 <matmon.2810@gmail.com>
2024-05-21 16:08:13 +0200

    Prima aggiunta
    Non funziona nulla

---
+++ StructTextArea.h
@@ -0,0 +1,17 @@
+#pragma once
+/*! \file StructTextArea.h
+	\brief Declaration of the struct TextArea
+
+	Details.
+*/
+
+
+# define SLEN 50
+
+/// @struct TextArea
+/// @brief includes the text to be showed and the font size 
+struct TextArea {
+	char string[SLEN]; // the text 
+	unsigned int size; // font size
+
+};
